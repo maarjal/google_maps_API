58e198bbb733f98c2a77bc20b521fa9bc29da7aa
Maarja Laineste and Noah Yasskin <noah@>
2016-08-03 11:13:11 -0700

    add google maps functionality

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,2 +1,41 @@
 var apiKey = require('./../.env').apiKey;
 var Map = require('./../js/map.js').mapModule;
+
+
+$( document ).ready(function() {
+  $('#locateEstonia').click(locateUser);
+});
+
+
+function locateUser() {
+
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
+}
