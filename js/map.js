6d48d30796b6c110e9c45adef2351c3f55c18b94
Maarja Laineste and Noah Yasskin <noah@>
2016-08-03 14:56:49 -0700

    move Epicodus and Estonia map logic to back-end

--- js/map.js
+++ js/map.js
@@ -1,3 +1,83 @@
 
+CustomMap = function() {
+};
 
-exports.mapModule = Map;
+CustomMap.prototype.locateEstonia = function(){
+  var map = new google.maps.Map(document.getElementById('estonia'), {
+    center: {lat: 59.432, lng: 24.721},
+    scrollwheel: false,
+    zoom: 6,
+    MapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+};
+
+// locate epicodus
+CustomMap.prototype.locateUser = function() {
+
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+  $('.maarjaNow').show();
+
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 18,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  var mapObject = new google.maps.Map(document.getElementById("epicodus"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
+}
+};
+
+// stree view of home street
+// function initPano() {
+//   var panorama = new google.maps.StreetViewPanorama(
+//    document.getElementById('map'), {
+//      pano: 'reception',
+//      visible: true,
+//      panoProvider: getCustomPanorama
+//  });
+// }
+//
+// function getCustomPanoramaTileUrl(pano, zoom, tileX, tileY) {
+//  return 'https://developers.google.com/maps/documentation/javascript/examples/full/images/panoReception1024-0.jpg';
+// }
+//
+// function getCustomPanorama(pano, zoom, tileX, tileY) {
+//  if (pano === 'reception') {
+//    return {
+//      location: {
+//        pano: 'reception',
+//        description: 'Google Sydney - Reception'
+//      },
+//      links: [],
+//      copyright: 'Imagery (c) 2010 Google',
+//      tiles: {
+//        tileSize: new google.maps.Size(1024, 512),
+//        worldSize: new google.maps.Size(1024, 512),
+//        centerHeading: 105,
+//        getTileUrl: getCustomPanoramaTileUrl
+//      }
+//    };
+//  }
+// }
+
+
+exports.mapModule = CustomMap;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,52 +1,15 @@
 var apiKey = require('./../.env').apiKey;
-var Map = require('./../js/map.js').mapModule;
+var CustomMap = require('./../js/map.js').mapModule;
 
 
-$( document ).ready(function() {
+$(document).ready(function() {
+  var epicodusMap = new CustomMap();
+  var estoniaMap = new CustomMap();
   $('.maarjaNow').hide();
-  $('#maarjaNow').click(locateUser);
-  $('#maarjaFrom').click(locateEstonia);
+  $('#maarjaNow').click(function() {
+    epicodusMap.locateUser();
+  });
+  $('#maarjaFrom').click(function() {
+    estoniaMap.locateEstonia();
+  });
 });
-
-function locateUser() {
-
-  if (navigator.geolocation){
-    var positionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10 * 1000
-    };
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
-  }
-  else {
-    alert("Your browser doesn't support the Geolocation API");
-  }
-  $('.maarjaNow').show();
-}
-
-function geolocationSuccess(position) {
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-  var myOptions = {
-    zoom : 18,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-  var mapObject = new google.maps.Map(document.getElementById("epicodus"), myOptions);
-  new google.maps.Marker({
-    map: mapObject,
-    position: userLatLng
-  });
-}
-
-function geolocationError(positionError) {
-  alert(positionError);
-}
-
-function locateEstonia() {
-  var map = new google.maps.Map(document.getElementById('estonia'), {
-    center: {lat: 59.432, lng: 24.721},
-    scrollwheel: false,
-    zoom: 6,
-    MapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-}
